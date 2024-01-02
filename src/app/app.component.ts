7706c24c26cac635d096bd1bd2c6f8229cfe8c2c
mohit-chauhan-stackpro <mohit.chauhan@stackpro.in>
2024-01-02 17:52:32 +0530

    added rete code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  AfterViewInit,
+  ViewChild,
+  ElementRef,
+  Injector,
+} from '@angular/core';
+import { createEditor } from './editor';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'rete-test-application';
+
+  constructor(private injector: Injector) {}
+
+  @ViewChild('rete') container!: ElementRef;
+
+  ngAfterViewInit(): void {
+    const el = this.container.nativeElement;
+
+    if (el) {
+      createEditor(el, this.injector);
+    }
+  }
 }
